95c4bb708d4d54da56b1b6e1554fd5f0cf3a4082
Kashalls <kashalls@pm.me>
2023-01-24 13:26:11 -0800

    cloudflare-worker

---
+++ projects/cloudflare-worker/src/index.js
@@ -0,0 +1,64 @@
+const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+
+export default {
+
+	async fetch(request, env) {
+		if (request.method === 'POST') {
+			const body = await request.formData();
+			const token = body.get('cf-turnstile-response');
+			const ip = request.headers.get('CF-Connecting-IP');
+
+			let formData = new FormData();
+			formData.append('secret', TURNSTILE_SECRET);
+			formData.append('response', token);
+			formData.append('remoteip', ip);
+
+			const result = await fetch(url, {
+				body: formData,
+				method: 'POST'
+			});
+
+			const outcome = await result.json();
+			if (outcome.success) {
+				const ExternalBody = {
+					username: body.get('username'),
+					message: body.get('message')
+				}
+
+				const response = await fetch(EXTERNAL_SERVER, {
+					body: ExternalBody,
+					method: 'POST',
+					headers: {
+						'content-type': 'application/json;charset=UTF-8',
+					}
+				})
+
+				if (response.success) {
+					return new Response(JSON.stringify({ success: true }), { status: 204 })
+				} else {
+					return new Response(JSON.stringify({ success: false, error: ['something-went-wrong-and-i-dont-know-what-to-do'] }), { status: 500 })
+				}
+			} else {
+				return new Response(JSON.stringify(outcome, { status: 401 }));
+			}
+		}
+
+		if (request.method === 'GET') {
+			const available = await env.AVAILABLE.get('available')
+			if (available === null) {
+				await env.AVAILABLE.put('available', false)
+			}
+			return new Response(JSON.stringify({ success: true, available: available === null ? false : Boolean(available) }))
+		}
+	}
+};
