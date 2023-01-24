30939cff7c474c7d47d3be952e5b30c21c698a7a
Kashalls <kashalls@pm.me>
2023-01-24 13:26:47 -0800

    Remove heading

--- projects/cloudflare-worker/src/index.js
+++ projects/cloudflare-worker/src/index.js
@@ -1,14 +1,4 @@
 const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 
 export default {
 
